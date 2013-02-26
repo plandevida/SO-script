b7b729ce32b710b3424f40b363e6de37010b9dc6
plandevida <registros_nano@hotmail.com>
2013-02-26 10:55:16 +0100

    Mensajes de error redirigidos
    Redirigidos los mensajes de error por la salida estándar de error.

--- quien.sh
+++ quien.sh
@@ -12,26 +12,27 @@ if [ -f $fichero ] ; then
 	echo ""	
 	echo "SCRIPT QUE OBTIENE LA INFORMACIÓN DE UN USUARIO UNIX DE UN FICHERO CON FORMATO passwd"
 	echo ""
-	echo -n "introduce el nombre del usuario (fin termina): "
+	echo -n "Introduce el nombre del usuario ('fin' termina): "
 	read usuario
 
 	while [ "$usuario" != "fin" ] ; do
 		
 		# Comprueba que el usuario existe en /etc/passwd
-		comando=`grep "^$usuario:" $fichero`
+		comando=`grep "^$usuario:" $fichero` 2> /dev/null
+
 		if [ $? -ne 0 ] ; then
-			echo "El usuario $usuario no existe en el fichero: $fichero."
-			exit 1
+
+			echo "El usuario $usuario no existe en el fichero: $fichero." 1>&2
 		else
 			echo ""
-
+			
 			login=`echo $comando | cut -d : -f 1`
 			nombre=`echo $comando | cut -d : -f 5`
 			gid=`echo $comando | cut -d : -f 4`
 			uid=`echo $comando | cut -d : -f 3`
 			home=`echo $comando | cut -d : -f 6`
 			shell=`echo $comando | cut -d : -f 7`
-
+			
 			echo "DATOS DEL USUARIO:"
 			echo "Login: $login"
 			echo "Nombre: $nombre"
@@ -40,13 +41,14 @@ if [ -f $fichero ] ; then
 			echo "home: $home"
 			echo "shell: $shell"
 		fi
-
-		echo -n "introduce el nombre del usuario: "
+		
+		echo -n "Introduce el nombre del usuario ('fin' termina): "
 		read usuario
 	done
 else
+	echo "El fichero no existe." 1>&2
 	
-	echo "El fichero no existe."
+	exit 2;
 fi
 
 exit 0
